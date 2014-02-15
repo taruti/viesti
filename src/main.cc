a2bb37828c5525e7a935e1d8b641d8fb0e466f42
Taru Karttunen <taruti@taruti.net>
2014-02-15 22:18:05 +0200

    Application name + organisation

--- src/main.cc
+++ src/main.cc
@@ -4,6 +4,9 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setOrganizationName("Violetti");
+    a.setOrganizationDomain("violetti.org");
+    a.setApplicationName("viesti");
     MainWindow w;
     w.show();
     return a.exec();
