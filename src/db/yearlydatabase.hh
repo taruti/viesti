991eb2120a37e77afe9dbc83b0f307b4e6fcc917
Taru Karttunen <taruti@taruti.net>
2014-02-16 18:12:30 +0200

    YearlyDatabase constructor

--- src/db/yearlydatabase.hh
+++ src/db/yearlydatabase.hh
@@ -12,11 +12,13 @@
 using Offset = std::int64_t;
 
 class YearlyDatabase {
+	std::string path_;
 	using map_type = boost::container::flat_map<std::string, std::string>;
 	map_type addrs_;
 	void add_addresses(vmime::shared_ptr<vmime::header> &hdr);
 	void add_mail_address(std::string, std::string);
 public:
+	YearlyDatabase(std::string path) : path_(path) {}
 	void add_message(vmime::message &msg);
 	int naddrs() const { return addrs_.size(); }
 };
