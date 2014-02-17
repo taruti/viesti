b0a4a9fb2af44154da4be3bdf9a80c094cfcc499
Taru Karttunen <taruti@taruti.net>
2014-02-17 05:22:53 +0200

    Digest serialization

--- src/util/digest.hh
+++ src/util/digest.hh
@@ -8,12 +8,18 @@ class Digest {
 	enum { SIZE = 20 };
 	unsigned char d_[SIZE];
 public:
-    explicit Digest(const std::string &hashme);
+	Digest() : d_{0} {}
+	explicit Digest(const std::string &hashme);
 
 	std::string qstring() const {
 		std::string res{1 + SIZE, 'Q'};
 		std::memcpy(&res[1], d_, SIZE);
 		return res;
+	}
+
+	template<class Archive>
+	void serialize(Archive & archive) {
+		archive(d_);
 	}
 
 };
