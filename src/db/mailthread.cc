5b7fb5921c74a79f27a3c9acfaef84ade227325f
Taru Karttunen <taruti@taruti.net>
2014-02-17 05:23:25 +0200

    Initial MailThread

---
+++ src/db/mailthread.cc
@@ -0,0 +1,85 @@
+#include <algorithm>
+#include <chrono>
+#include <cstring>
+#include <sstream>
+#include <cereal/archives/portable_binary.hpp>
+#include <cereal/types/string.hpp>
+#include <cereal/types/vector.hpp>
+extern "C" {
+#include <time.h>
+}
+
+#include "globals.hh"
+#include "mailthread.hh"
+ 
+void MailThread::add_message(i64 id, const vmime::message &msg) {
+    if(std::find(msgs_.begin(), msgs_.end(), id) != msgs_.end())
+        return;
+    msgs_.push_back(id);
+    
+    auto hdr = msg.getHeader();
+    mids_.emplace_back(hdr->MessageId()->getValue<vmime::messageId>()->getId());
+
+    auto ldate = hdr->Date()->getValue<vmime::datetime>();
+    struct std::tm tm;
+    std::memset(&tm, 0, sizeof(tm));
+    int zonemin;
+    ldate->getDate(tm.tm_year,tm.tm_mon,tm.tm_mday);
+    ldate->getTime(tm.tm_hour,tm.tm_min,tm.tm_sec,zonemin);
+    tm.tm_year -= 1900;
+    tm.tm_min += zonemin;
+    auto utc = timegm(&tm);
+    bool newest = utc > utc_date_;
+
+    if(newest) {
+        utc_date_ = utc;
+
+        auto subj = hdr->Subject()->getValue<vmime::text>()->getConvertedText(utf8);
+        if(subj.size())
+            subject_ = subj;
+    }
+
+	auto from = hdr->From()->getValue<vmime::mailbox>();
+	auto ft = from->getName();
+	std::string sfrom{from->getEmail().toString()};
+	if(!ft.isEmpty())
+		sfrom = ft.getConvertedText(utf8) + " <" + sfrom + ">";
+	froms_.push_back(sfrom);
+}
+
+void MailThread::join(const MailThread &o) {
+    if(o.utc_date_ > utc_date_) {
+        utc_date_ = o.utc_date_;
+        subject_ = o.subject_;
+    }
+    for(auto &&e : o.froms_)
+        froms_.push_back(e);
+    for(auto &&e : o.msgs_)
+        msgs_.push_back(e);
+    for(auto &&e : o.mids_)
+        mids_.push_back(e);
+}
+
+std::string MailThread::date() const {
+    struct tm tm;
+    localtime_r(&utc_date_, &tm);
+    std::string s(32, '\0');
+    s.resize(strftime(&s[0], 32, "%F %R", &tm));
+    return s;
+}
+
+std::string MailThread::encode() const {
+    std::stringstream ss;
+    cereal::PortableBinaryOutputArchive oa{ss};
+    oa(*this);
+    return ss.str();
+}
+
+MailThread MailThread::decode(const std::string &s) {
+    std::stringstream ss{s};
+    cereal::PortableBinaryInputArchive ia{ss};
+    MailThread mt;
+    ia(mt);
+    return mt;
+}
+
--- src/db/mailthread.hh
+++ src/db/mailthread.hh
@@ -3,13 +3,52 @@
 
 #include <string>
 #include <vector>
+#include "../util/common.hh"
+#include "../util/digest.hh"
 
 class MailThread {
     std::time_t utc_date_ = 0;
     std::string subject_ = "";
-    std::vector<std::string> froms_;
-    std::vector<i64> msgs_;
-    std::vector<std::string> messageids_;
+    std::vector<std::string> froms_ = {};
+    std::vector<i64> msgs_ = {};
+	std::vector<Digest> mids_ = {};
+public:
+    MailThread() {}
+    MailThread(const MailThread&) = delete;
+    MailThread(MailThread &&o) noexcept {
+        utc_date_ = o.utc_date_;
+        subject_ = std::move(o.subject_);
+        froms_ = std::move(o.froms_);
+        msgs_ = std::move(o.msgs_);
+        mids_ = std::move(o.mids_);
+    }
+    MailThread& operator=(MailThread&& o) noexcept {
+        utc_date_ = o.utc_date_;
+        subject_ = std::move(o.subject_);
+        froms_ = std::move(o.froms_);
+        msgs_ = std::move(o.msgs_);
+        mids_ = std::move(o.mids_);
+        return *this;
+    }
+    bool contains(const Digest &qmid) const;
+    void add_message(i64 id, const vmime::message &msg);
+    void join(const MailThread &thread);
+    std::string date() const;
+	std::vector<std::string> from() const {
+        return froms_;
+    }
+    std::string subject() const {
+        return subject_;
+    }
+
+	template<class Archive>
+    void serialize(Archive & archive)
+    {
+        archive(utc_date_, subject_, froms_, msgs_, mids_);
+    }
+
+	std::string encode() const;
+    static MailThread decode(const std::string &s);
 };
 
 #endif /* MAILTHREAD_HH */
