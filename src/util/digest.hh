b8a15c9c56525d61095a8a9bd52ca2c1b3f99316
Taru Karttunen <taruti@taruti.net>
2014-02-17 05:01:51 +0200

    Digest.qstring

--- src/util/digest.hh
+++ src/util/digest.hh
@@ -2,11 +2,20 @@
 #define DIGEST_HH 
 
 #include <string>
+#include <cstring>
 
 class Digest {
-    unsigned char d_[20];
+	enum { SIZE = 20 };
+	unsigned char d_[SIZE];
 public:
     explicit Digest(const std::string &hashme);
+
+	std::string qstring() const {
+		std::string res{1 + SIZE, 'Q'};
+		std::memcpy(&res[1], d_, SIZE);
+		return res;
+	}
+
 };
 
 #endif /* DIGEST_HH */
